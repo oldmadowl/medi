d73ae8f35ec7a88e18fd4af955cf9500d98fcce4
oldmadowl <oldmadowl@gmail.com>
2022-06-08 22:21:44 +0300

    added section offer

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 $(function(){
 
-  
+  $('.offer__slider').slick({
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/prev-arrow.svg" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/next-arrow.svg" alt=""></button>',
+  });
 
 });
